da6bc8ee33bd5eb7dcbe4966326c9ee1b9da216f
mobihack <mobihack.me@gmail.com>
2019-06-14 11:51:36 +0530

    Fixed Autoprefixer problem. (Closes: #6) Bug fixes and performance improvements.

--- src/js/build.js
+++ src/js/build.js
@@ -20,20 +20,7 @@ const header = `/*!
 `
 
 const autoPrefixerConfig = {
-  cascade: false,
-  browsers: [
-    '>= 1%',
-    'last 1 major version',
-    'not dead',
-    'Chrome >= 45',
-    'Firefox >= 38',
-    'Edge >= 12',
-    'Explorer >= 10',
-    'iOS >= 9',
-    'Safari >= 9',
-    'Android >= 4.4',
-    'Opera >= 30'
-  ]
+  cascade: false
 }
 
 const configCleanCSS = {
